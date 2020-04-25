96d15ff657e6eadf72d989c3f3a923010b3185d1
Cufee <vkouzin@outlook.com>
2020-04-25 10:23:10 -0700

    added purgetext command and selfie channel timer

--- cogs/cmd_secret_chats.py
+++ cogs/cmd_secret_chats.py
@@ -1,4 +1,6 @@
 import discord
+import re
+import asyncio
 from discord.ext import commands
 
 # Setup Guild
@@ -19,9 +21,38 @@ class secret_chats(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        pass
+        channel = message.channel
+        message_text = message.clean_content
+        if channel.name == 'selfie-channel' and ':beta_feature:' not in message_text:
+            attachments = message.attachments
+            re_filter = re.compile('https:\/\/....')
+            if re_filter.match(message_text):
+                attachments.append('link found')
+            if not attachments:
+                await asyncio.sleep(1800)
+                await message.delete()
+            else:
+                emoji = discord.utils.get(
+                    message.guild.emojis, name='beta_feature')
+                message.add_reaction(emoji)
 
     # Commands
+    @commands.command(aliases=['pt'])
+    async def purgetext(self, ctx, message_count):
+        channel = ctx.channel
+        if channel.name == 'selfie-channel':
+            async for message in channel.history(limit=int(message_count)):
+                message_text = message.clean_content
+                attachments = message.attachments
+
+                re_filter = re.compile('https:\/\/....')
+                if re_filter.match(message_text):
+                    attachments.append('link found')
+
+                if not attachments:
+                    await message.delete()
+                else:
+                    pass
 
 
 def setup(client):
