35fb9714a69e07a93d3bd35b1c7f3a26ff7ae81e
Cufee <vkouzin@outlook.com>
2020-04-25 07:55:57 -0700

    first version of upvotes

--- cogs/cmd_message_votes.py
+++ cogs/cmd_message_votes.py
@@ -2,6 +2,12 @@ import discord
 from discord.ext import commands, tasks
 
 
+async def get_vote_channels(guild_id):
+    test_channel = 'memes'
+    vote_channels = [test_channel]
+    return vote_channels
+
+
 class message_votes(commands.Cog):
 
     def __init__(self, client):
@@ -17,9 +23,18 @@ class message_votes(commands.Cog):
     # @commands.Cog.listener()
     @commands.Cog.listener()
     async def on_message(self, message):
-        channel = message.channel
+        channel = message.channel.name
         attachments = message.attachments
-        print(attachments)
+        vote_channels = get_vote_channels('guild_id here')
+        if channel in vote_channels and attachments:
+            print('new message in memes with an attachment')
+            emotes = [discord.utils.get(message.guild.emojis, name='upvote'), discord.utils.get(
+                message.guild.emojis, name='downvote')]
+            for emote in emotes:
+                message.add_reaction(emote)
+                print(f'added {emote}')
+        else:
+            pass
 
 
 def setup(client):
