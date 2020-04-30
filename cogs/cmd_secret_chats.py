b10d7ffae7ef7cb3b3fce191458b4255840d7727
Cufee <vkouzin@outlook.com>
2020-04-30 10:32:49 -0700

    Set reaction delay to 120 sec

--- cogs/cmd_secret_chats.py
+++ cogs/cmd_secret_chats.py
@@ -30,14 +30,10 @@ class secret_chats(commands.Cog):
             if re_filter.match(message_text):
                 attachments.append('link found')
             if not attachments:
-                # Waiting 24 hours before deleting message
-                await asyncio.sleep(84600)
+                await asyncio.sleep(120)
                 emoji = discord.utils.get(
                     message.guild.emojis, name='remove')
-                # Adding a reaction 30 minutes before deletion
                 await message.add_reaction(emoji)
-                # await asyncio.sleep(1800)
-                # await message.delete()
             else:
                 emoji = discord.utils.get(
                     message.guild.emojis, name='saved')
