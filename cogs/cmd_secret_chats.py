18c6b5c8cec92c5d0bfa1a643afbbebddac51c7b
Cufee <vkouzin@outlook.com>
2020-04-30 00:00:46 -0700

    moved some loginc around

--- cogs/cmd_secret_chats.py
+++ cogs/cmd_secret_chats.py
@@ -30,13 +30,13 @@ class secret_chats(commands.Cog):
             if re_filter.match(message_text):
                 attachments.append('link found')
             if not attachments:
+                # Waiting 24 hours before deleting message
+                await asyncio.sleep(84600)
                 emoji = discord.utils.get(
                     message.guild.emojis, name='trg_removing30')
                 # Adding a reaction 30 minutes before deletion
-                await asyncio.sleep(1800)
                 await message.add_reaction(emoji)
-                # Waiting 24 hours before deleting message
-                await asyncio.sleep(84600)
+                await asyncio.sleep(1800)
                 await message.delete()
             else:
                 emoji = discord.utils.get(
